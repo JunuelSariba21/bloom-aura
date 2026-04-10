22071288c6982b6a7853930a57883bf71116f599
aleahjalawig <jalawig0613@example.com>
2026-04-11 03:02:29 +0800

    4

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -8,5 +8,9 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'category/:id',
     renderMode: RenderMode.Server
-  }
+  },
+  {
+    path: 'flower/:id',
+    renderMode: RenderMode.Server
+  },
 ];
