652c521c84f26f035c82b4381e6c5e97da874153
aleahjalawig <jalawig0613@example.com>
2026-04-11 00:06:57 +0800

    login credentials

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  username = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username === 'admin' && this.password === '1234') {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+}
